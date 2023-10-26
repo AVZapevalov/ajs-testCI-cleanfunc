aa55b90bde68af8d37cf5324d4caca63bf365e2f
AVZapevalov <106755729+AVZapevalov@users.noreply.github.com>
2023-10-26 19:40:37 +0100

    init

---
+++ src/__tests__/test.js
@@ -0,0 +1,16 @@
+import getHealthStatus from '../getHealthStatus';
+
+test('getHealthStatus returns "healthy" when health is greater than 50', () => {
+  const result = getHealthStatus({ name: 'Маг', health: 60 });
+  expect(result).toBe('healthy');
+});
+
+test('getHealthStatus returns "wounded" when health is between 15 and 50', () => {
+  const result = getHealthStatus({ name: 'Лучник', health: 30 });
+  expect(result).toBe('wounded');
+});
+
+test('getHealthStatus returns "critical" when health is less than 15', () => {
+  const result = getHealthStatus({ name: 'Паладин', health: 10 });
+  expect(result).toBe('critical');
+});
